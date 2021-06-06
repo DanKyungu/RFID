5382015f4a7745947733ca82f1d6c9125bbf27f3
dankyungu <kyungu.dan@hotmail.com>
2021-06-06 22:14:18 +0200

    * .gitignore: Initial Commit
    * RFID.sln:
    * App.xaml:
    * RFID.csproj:
    * App.xaml.cs:
    * Main.cs:
    * MainPage.xaml:
    * Info.plist:
    * AssemblyInfo.cs:
    * MainPage.xaml.cs:
    * AppDelegate.cs:
    * RFID.iOS.csproj:
    * Entitlements.plist:
    * MainActivity.cs:
    * Default.png:
    * res.flag:
    * RFID.Android.csproj:
    * R.cs.flag:
    * Default@2x.png:
    * AssemblyInfo.cs:
    * AboutAssets.txt:
    * Default-568h@2x.png:
    * AssemblyInfo.cs:
    * Default-Portrait.png:
    * styles.xml:
    * colors.xml:
    * customview-map.txt:
    * AboutResources.txt:
    * LaunchScreen.storyboard:
    * Default-Portrait@2x.png:
    * AndroidManifest.xml:
    * Resource.designer.cs:
    * icon.png:
    * icon.png:
    * icon.png:
    * colors.xml:
    * styles.xml:
    * icon.png:
    * build.props:
    * icon.png:
    * icon.png:
    * icon.png:
    * icon.png:
    * icon.xml:
    * icon.png:
    * icon.png:
    * values_colors.arsc.flat:
    * values_styles.arsc.flat:
    * mipmap-hdpi_icon.png.flat:
    * icon.xml:
    * mipmap-mdpi_icon.png.flat:
    * Icon40.png:
    * libraryimports.cache:
    * Icon29.png:
    * Icon87.png:
    * Resource.designer.cs:
    * Icon80.png:
    * mipmap-xhdpi_icon.png.flat:
    * Icon76.png:
    * Icon60.png:
    * Icon58.png:
    * Icon20.png:
    * mipmap-xxhdpi_icon.png.flat:
    * icon_round.xml:
    * Icon180.png:
    * Icon167.png:
    * Icon152.png:
    * Icon120.png:
    * Icon1024.png:
    * mipmap-xxxhdpi_icon.png.flat:
    * Contents.json:
    * _ConvertResourcesCases.stamp:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * icon_round.xml:
    * mipmap-anydpi-v26_icon.xml.flat:
    * _BuildLibraryImportsCache.stamp:
    * launcher_foreground.png:
    * libraryprojectimports.cache:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * _ResolveLibraryProjectImports.stamp:
    * launcher_foreground.png:
    * mipmap-anydpi-v26_icon_round.xml.flat:
    * RFID.Android.csprojAssemblyReference.cache:
    * launcher_foreground.png:
    * mipmap-mdpi_launcher_foreground.png.flat:
    * mipmap-hdpi_launcher_foreground.png.flat:
    * mipmap-xhdpi_launcher_foreground.png.flat:
    * mipmap-xxhdpi_launcher_foreground.png.flat:
    * mipmap-xxxhdpi_launcher_foreground.png.flat:
    * RFID.iOS.csprojAssemblyReference.cache:

---
+++ RFID/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.CommunityToolkit;
+using Xamarin.CommunityToolkit.UI.Views.Options;
+using Xamarin.CommunityToolkit.Extensions;
+using Plugin.NFC;
+
+namespace RFID
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            if(CrossNFC.Current.IsAvailable && CrossNFC.Current.IsEnabled)
+            {
+                CrossNFC.Current.OnMessageReceived += Current_OnMessageReceived;
+                CrossNFC.Current.OnTagDiscovered += Current_OnTagDiscovered;
+                CrossNFC.Current.OnNfcStatusChanged += Current_OnNfcStatusChanged;
+            }
+        }
+
+        private async void Current_OnNfcStatusChanged(bool isEnabled)
+        {
+            await this.DisplayToastAsync($"NFC : {isEnabled}", 5000);
+        }
+
+        private async void Current_OnTagDiscovered(ITagInfo tagInfo, bool format)
+        {
+            await this.DisplayToastAsync("Data was read successfully", 5000);
+        }
+
+        private async void Current_OnMessageReceived(ITagInfo tagInfo)
+        {
+            await this.DisplayToastAsync(tagInfo.SerialNumber, 5000);
+        }
+
+        async void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            await this.DisplayToastAsync("Data persisted successfully", 5000);
+        }
+
+        async void Button_Clicked_1(System.Object sender, System.EventArgs e)
+        {
+            await this.DisplayToastAsync("Data was read successfully", 5000);
+        }
+    }
+}
